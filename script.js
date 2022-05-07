b9648b6b0d64739a3b1c02290ab4a38e12493d8d
Weissey <kelvinchangw@gmail.com>
2022-05-07 14:45:18 -0700

    V2 of Etch-A-Sketch's script code completed

--- script.js
+++ script.js
@@ -1,92 +1,103 @@
-let colFragment = new DocumentFragment();
-const buttons = document.querySelectorAll("button");
-const input = document.querySelector("input");
+const container = document.getElementById("container");
+const leftContainer = document.getElementById("leftContainer");
+const sketchGrid = document.getElementById("sketchGrid");
+const title = document.getElementById("title");
+const footer = document.getElementById("footer");
+const colorSquare = document.getElementById("colorSquare");
+const axisNumber = document.getElementById("axisNumber");
+const normalButton = document.getElementById("normalButton");
+const rainbowButton = document.getElementById("rainbowButton");
+const clearButton = document.getElementById("clearButton");
+const sizeSlider = document.getElementById("sizeSlider");
+const displaySize = document.getElementById("displaySize");
 
-buttons.forEach(button => button.addEventListener("click", checkButtonChoice))
+const DEFAULT_BRUSH_COLOR = "#1c1c1c"
+const DEFAULT_GRID_SIZE = 16
+const DEFAULT_MODE = "color"
 
-setGrid(16);
+let currentColor = DEFAULT_BRUSH_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_GRID_SIZE;
 
-//Function to reset and create new grids
-function setGrid(e) {
-    let toBeDeleted = document.querySelectorAll(".column");
-    toBeDeleted.forEach(div => div.remove());
-    for (let i = 0; i < e; i++) {
-        let div = document.createElement("div");
-        for (let i = 0; i < e; i++) {
-            let tempDiv = document.createElement("div");
-            tempDiv.classList.add("row");
-            div.appendChild(tempDiv);
+let mousePress = false;
+document.body.onmousedown = () => (mousePress = true);
+document.body.onmouseup = () => (mousePress = false);
+
+normalButton.onclick = () => setCurrentMode("color");
+rainbowButton.onclick = () => setCurrentMode("rainbow");
+clearButton.onclick = () => reconstructGrid();
+colorSquare.onchange = (e) => setCurrentColor(e.target.value);
+sizeSlider.onmousemove = (e) => updateSizeDisplay(e.target.value);
+
+function constructGrid(size) {
+    sketchGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    sketchGrid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const tempGridElement = document.createElement("div");
+        tempGridElement.classList.add("grid-block");
+        tempGridElement.addEventListener('mouseover', changeColor);
+        tempGridElement.addEventListener('mousedown', changeColor);
+        sketchGrid.appendChild(tempGridElement);
+    }
+}
+
+sizeSlider.onchange = (e) => changeGrid(e.target.value);
+
+function changeGrid(value) {
+    setCurrentSize(value);
+    updateSizeDisplay(value);
+    reconstructGrid();
+}
+
+function setCurrentSize(newSizeValue) {
+    currentSize = newSizeValue;
+}
+
+function setCurrentMode(newMode) {
+    currentMode = newMode;
+}
+
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
+
+function updateSizeDisplay(value) {
+    displaySize.innerHTML = `${value} X ${value}`;
+}
+
+function reconstructGrid() {
+    clearGrid();
+    constructGrid(currentSize);
+}
+
+function clearGrid() {
+    sketchGrid.innerHTML = " ";
+}
+
+function changeColor(e) {
+    console.log(e.type);
+    if (e.type === "mouseover" && !mousePress) return;
+    if (currentMode === "color") {
+        e.target.style.backgroundColor = currentColor;
+    } else if (currentMode === "rainbow") {
+        e.target.style.backgroundColor = generateHex();
+    }
+}
+
+function generateHex() {
+    let hex = "#";
+    const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+        for (i = 0; i < 6; i++) {
+            const index = Math.floor(Math.random() * hexValues.length);
+            hex += hexValues[index];
         }
-        div.classList.add("column");
-        colFragment.appendChild(div);
-        document.querySelector("div").appendChild(colFragment);
-    }
-    activateNormalColor();
+    return hex;
 }
 
 
-//Color switch functions activated by buttons below
-function activateNormalColor() {
-    const divs = document.querySelectorAll(".row");
-    document.querySelector(".normal-button").style.cssText = `
-        transition: all 0.2s ease-in;
-        filter: brightness(70%);
-        `;
-    divs.forEach(div => div.addEventListener("mouseover", function () {
-        this.style.backgroundColor = "rgb(200, 200, 200)";
-    }));
-    return;
-}
 
-function activateRainbowColor() {
-    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    const divs = document.querySelectorAll(".row");
-    divs.forEach(div => div.addEventListener("mouseover", function () {
-        this.style.backgroundColor = `#${randomColor}`;
-        console.log(this.style.backgroundColor + "RAINBOW");
-        randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    }));
-    return;
-}
-
-//Function to determine if input for setGrid is correct
-function changeSize() {
-    let answer = document.querySelector(".axis-number").value;
-    console.log(answer);
-    if (answer == 0 || answer === "") {
-        setGrid(16);
-        return;
-    }   else if ((isNaN(answer) !== true) && answer <= 100) {
-        setGrid(answer);
-        return;
-    } else if ((isNaN(answer) !== true) && answer > 100) {
-        alert("The number you have entered is above the limit (100).");
-        setGrid(16);
-        return;
-    }
-}
-
-input.addEventListener("keyup", changeSize);
-
-
-//Code bloat but currently have no way of optimization May/4/2022
-function checkButtonChoice() {
-    console.log(this.classList.value)
-    if (this.classList.value == "normal-button") {
-        this.style.cssText = `
-        transition: all 0.2s ease-in;
-        filter: brightness(70%);
-        `;
-        document.querySelector(".rainbow-button").removeAttribute("style", "filter");
-        activateNormalColor();
-        return;
-    } else if (this.classList.value === "rainbow-button") {
-        this.style.cssText = `
-        transition: all 0.2s ease-in;
-        filter: brightness(70%);
-        `;
-        document.querySelector(".normal-button").removeAttribute("style", "filter");
-        activateRainbowColor();
-        return;
-    } 
-}
+window.onload = () => {
+    constructGrid(DEFAULT_GRID_SIZE);
+    //activateButton(DEFAULT_MODE)
+  }
